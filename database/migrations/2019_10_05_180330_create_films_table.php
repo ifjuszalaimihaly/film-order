624147eb8cd855e8213c855a18010118e2bcfe49
ifjuszalaimihaly <ifjuszalaimihaly@gmail.com>
2019-10-05 20:54:24 +0200

    created orders and films mingrations and models

--- database/migrations/2019_10_05_180330_create_films_table.php
+++ database/migrations/2019_10_05_180330_create_films_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateFilmsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('films', function (Blueprint $table) {
             $table->charset = 'utf8';
             $table->collation = 'utf8_hungarian_ci';
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('original_title')->nullable();
+            $table->unsignedSmallInteger('release_year')->nullable();
+            $table->string('imdb',10)->nullable();
+            $table->unsignedTinyInteger('rating')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('films');
     }
 }
---
+++ database/migrations/2019_10_05_182442_create_orders_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_hungarian_ci';
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('film_id');
+            $table->boolean('owner')->default(false);
+            $table->enum('status',
+                ['unchecked','accepted','refused','downloading','downloaded','burned','unattainable']
+            );
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users');
+
+            $table->foreign('film_id')
+                ->references('id')->on('films');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
